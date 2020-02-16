d8e2e1d40e4b3f22e5ff9dcdcd1840f40d6b946f
Deian Radev <deian.radev@perficient.com>
2020-02-15 19:12:33 -0500

    adding firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDzIknmrSwNaEnfrraTelkEFCXxDqN6pcc",
+  authDomain: "find-fido-a2392.firebaseapp.com",
+  databaseURL: "https://find-fido-a2392.firebaseio.com",
+  projectId: "find-fido-a2392",
+  storageBucket: "find-fido-a2392.appspot.com",
+  messagingSenderId: "664041683039",
+  appId: "1:664041683039:web:ffc8db3b9100b00a7483ea"
 };
 
 /*
