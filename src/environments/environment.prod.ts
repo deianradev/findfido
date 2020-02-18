984174e1f1a77af6a5c8c26522b06c6b96c2aa40
Deian Radev <deian.radev@perficient.com>
2020-02-18 11:38:21 -0500

    add firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDzIknmrSwNaEnfrraTelkEFCXxDqN6pcc",
+    authDomain: "find-fido-a2392.firebaseapp.com",
+    databaseURL: "https://find-fido-a2392.firebaseio.com",
+    projectId: "find-fido-a2392",
+    storageBucket: "find-fido-a2392.appspot.com",
+    messagingSenderId: "664041683039",
+    appId: "1:664041683039:web:ffc8db3b9100b00a7483ea"
+  }
 };
